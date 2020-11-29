c62174e842012cc18d644dd6dd123cdaff8214fd
Lis Maria Saju <lismariasaju@gmail.com>
2020-11-29 18:17:40 +0530

    sign-up files

--- mod5_soln/src/public/public.routes.js
+++ mod5_soln/src/public/public.routes.js
@@ -40,6 +40,17 @@ function routeConfig ($stateProvider) {
           return MenuService.getMenuItems($stateParams.category);
         }]
       }
+    })
+    .state('public.signup',{
+      url: '/signup',
+      templateUrl: 'src/public/sign-up/sign-up.html',
+      controller: 'SignUpController',
+      controllerAs: 'signUpCtrl',
+      resolve: {
+        menuItems: ['$stateParams','MenuService', function ($stateParams, MenuService) {
+          return MenuService.getMenuItems($stateParams.category);
+        }]
+      }
     });
 }
 })();
